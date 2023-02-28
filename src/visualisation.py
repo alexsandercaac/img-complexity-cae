7d015bdd8322e58c9d5cc1794b5f0641f934970c
Alexsander <alexsandercaac@gmail.com>
2023-02-28 18:05:11 -0300

    Added visualisation of reconstructed images

--- src/visualisation.py
+++ src/visualisation.py
@@ -3,11 +3,14 @@
     specified by the user. The images will be saved in the folder
     data/interim/augmentation_visualisation.
 """
-from utils.data.tfdatasets import load_tf_img_dataset, augmentation_model
-from utils.dvc.params import get_params
 import matplotlib.pyplot as plt
 import os
 import logging
+import tensorflow as tf
+
+from utils.data.tfdatasets import load_tf_img_dataset, augmentation_model
+from utils.dvc.params import get_params
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # FATAL
 logging.getLogger('tensorflow').setLevel(logging.FATAL)
 
@@ -50,3 +53,34 @@ for images in dataset:
     break
 
 plt.savefig('visualisation/augmentation/augmented_images.png')
+
+# Get reconstruction of images and save them beside the original
+
+dataset = load_tf_img_dataset(
+    dir='val',
+    dir_path='data/processed',
+    input_size=tuple(params['input_size'])[:2],
+    mode='autoencoder',
+    scale=255,
+    shuffle=True,
+    augmentation=augmentation,
+    batch_size=2,
+    color_mode='grayscale'
+)
+
+model = tf.keras.models.load_model('models/best_cae.hdf5')
+
+plt.figure(figsize=(10, 10))
+for images, _ in dataset:
+    for i in range(2):
+        ax = plt.subplot(2, 2, i + 1)
+        plt.imshow(model(images)[i].numpy(), cmap='gray')
+        plt.title('Reconstructed')
+        plt.axis("off")
+        ax = plt.subplot(2, 2, i + 3)
+        plt.imshow(images[i].numpy(), cmap='gray')
+        plt.title('Original')
+        plt.axis("off")
+    break
+
+plt.savefig('visualisation/reconstruction/reconstructed_images.png')
--- visualisation/dag.md
+++ visualisation/dag.md
@@ -1,10 +1,15 @@
 ```mermaid
 flowchart TD
-	node1["augmentation_visualisation"]
-	node2["data/raw/casting_512x512.dvc"]
-	node3["extract_complexity"]
-	node4["train_test_split"]
-	node2-->node4
+	node1["cae_hp_tuning"]
+	node2["cae_training"]
+	node3["data/raw/casting_512x512.dvc"]
+	node4["dataset_split"]
+	node5["extract_complexity"]
+	node6["visualisation"]
+	node1-->node2
+	node3-->node4
 	node4-->node1
-	node4-->node3
+	node4-->node2
+	node4-->node5
+	node4-->node6
 ```
