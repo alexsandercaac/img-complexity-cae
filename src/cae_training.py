47f71ef131fd6212ff4bf570c4be19273d2149bf
Alexsander <alexsandercaac@gmail.com>
2023-02-27 23:12:26 -0300

    Trained CAE

---
+++ src/cae_training.py
@@ -0,0 +1,127 @@
+"""
+    This stage loads the model trained in the previous stage of hyperparameter
+    tuning and trains it on the training dataset. The model is trained for a
+    number of epochs and the best model is saved. The training history is
+    saved as a csv file and the model is saved as an hdf5 file.
+"""
+import os
+import io
+import logging
+
+import tensorflow as tf
+from tqdm.keras import TqdmCallback
+
+from utils.data.tfdatasets import load_tf_img_dataset, augmentation_model
+from utils.dvc.params import get_params
+from utils.models.kerascallbacks import CustomLearningRateScheduler
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # FATAL
+logging.getLogger('tensorflow').setLevel(logging.FATAL)
+
+# * Parameters
+
+stage_params = get_params()
+all_params = get_params('all')
+
+params = {**stage_params, **all_params}
+
+# * Augmentation
+
+augmentation = augmentation_model(
+    random_crop=tuple(params['random_crop']),
+    random_flip=params['random_flip'],
+    random_rotation=params['random_rotation'],
+    random_zoom=tuple(params['random_zoom']),
+    random_brightness=params['random_brightness'],
+    random_contrast=params['random_contrast'],
+    random_translation_height=tuple(params['random_translation_height']),
+    random_translation_width=tuple(params['random_translation_width'])
+)
+
+# * Load dataset
+train_dataset = load_tf_img_dataset(
+    dir='train',
+    dir_path='data/processed',
+    input_size=tuple(params['input_size'])[:2],
+    mode='autoencoder',
+    scale=255,
+    shuffle=True,
+    augmentation=augmentation,
+    batch_size=params['batch_size'],
+    color_mode='grayscale'
+)
+
+val_dataset = load_tf_img_dataset(
+    dir='val',
+    dir_path='data/processed',
+    input_size=tuple(params['input_size'])[:2],
+    mode='autoencoder',
+    scale=255,
+    shuffle=True,
+    augmentation=augmentation,
+    batch_size=params['batch_size'],
+    color_mode='grayscale'
+)
+
+model = tf.keras.models.load_model(
+    filepath='models/logs/hp_search_best.hdf5'
+)
+model.compile(loss=['mse'],
+              optimizer=tf.keras.optimizers.Adam(
+    learning_rate=params['learning_rate']),
+    metrics=['mae', 'mse']
+)
+lr_schedule = CustomLearningRateScheduler(
+    metric='val_mse',
+    secondary_metric='val_mae',
+    alpha=params['alpha'],
+    beta=params['beta'],
+    verbose=2,
+    patience=params['patience'],
+    early_stopping=params['early_stopping'],
+    revive_best=params['revive_best'],
+    min_lr=params['min_lr']
+)
+
+train_size = sum(
+    [len(files) for _, _, files in os.walk('data/processed/train')])
+
+print(f'Training samples: {train_size}')
+
+# Evaluate model on validation dataset before training
+print('Evaluating model on validation dataset before training...')
+val_loss_pre, val_mae_pre, val_mse_pre = model.evaluate(val_dataset, verbose=2)
+
+# * Train model
+
+history = model.fit(
+    train_dataset,
+    epochs=params['epochs'],
+    validation_data=val_dataset,
+    callbacks=[lr_schedule,
+               TqdmCallback(verbose=2,
+                            data_size=train_size,
+                            batch_size=params['batch_size'],
+                            epochs=params['epochs'])],
+    verbose=2
+)
+
+# Evaluate model on validation dataset after training
+print('Evaluating model on validation dataset after training...')
+val_loss_post, val_mae_post, val_mse_post = model.evaluate(
+    val_dataset, verbose=2)
+
+# * Save model and history
+
+if val_mse_post > val_mse_pre:
+    print('Validation loss increased after training.')
+    print('Reverting to best model...')
+    model = tf.keras.models.load_model(
+        filepath='models/logs/best_cae.hdf5')
+    model.save(filepath='models/best_cae.hdf5')
+else:
+    print('Validation loss decreased after training.')
+    print('Saving model...')
+    model.save(filepath='models/best_cae.hdf5')
+    with io.open('models/logs/history.csv', 'w') as f:
+        f.write(str(history.history))
