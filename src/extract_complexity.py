f6abcb275c23466a2d33d3679724d6ce57554d35
Alexsander <alexsandercaac@gmail.com>
2023-04-14 20:55:37 -0300

    Updated extract complexity stage

--- src/extract_complexity.py
+++ src/extract_complexity.py
@@ -3,22 +3,55 @@
 """
 from utils.data.jpegmse import calculate_jpeg_mse
 from utils.data.complexityaux import load_imgs_gen
-import os
+from utils.dvc.params import get_params
+from utils.data.filemanipulation import create_dir
+
 from tqdm.rich import tqdm
 from matplotlib import pyplot as plt
 
+import os
 
-with open('data/processed/tabular/complexity.csv', 'w') as f:
+import matplotlib.ticker as ticker
+import numpy as np
+
+
+params = get_params()
+stage_params = get_params()
+all_params = get_params('all')
+
+params = {**stage_params, **all_params}
+
+DATASET = params['dataset']
+GRAYSCLAE = params['grayscale']
+SCALE = params['scale']
+
+tabular_data_dir = os.path.join('data', 'processed', DATASET, 'tabular')
+create_dir(tabular_data_dir)
+
+file_name = 'complexity.csv'
+
+with open(os.path.join(tabular_data_dir, file_name), 'w') as f:
     f.write('file,jpeg_mse,data_split,label\n')
 
 DATA_SPLITS_AND_LABELS = [(split, label) for split in ['train', 'val', 'test']
-                          for label in ['ok_front', 'def_front']]
+                          for label in ['positive', 'negative']]
+
+fig, ax = plt.subplots()
+cmap = plt.get_cmap('cool')
+
+# Define colour dictionary
+colours = {
+    ('train', 'positive'): cmap(0.1),
+    ('val', 'positive'): cmap(0.2),
+    ('train', 'negative'): cmap(0.8),
+    ('val', 'negative'): cmap(0.9),
+}
 
 for split, label in DATA_SPLITS_AND_LABELS:
-    files = [os.path.join(f'data/processed/{split}/{label}', f)
-             for f in os.listdir(f'data/processed/{split}/{label}')]
+    data_dir = os.path.join('data', 'processed', DATASET, split, label)
+    files = [os.path.join(data_dir, f) for f in os.listdir(data_dir)]
 
-    img_gen = load_imgs_gen(files, grayscale=True, scale=255)
+    img_gen = load_imgs_gen(files, grayscale=GRAYSCLAE, scale=SCALE)
 
     mses = []
     pbar = tqdm(total=len(files))
@@ -27,13 +60,20 @@ for split, label in DATA_SPLITS_AND_LABELS:
         pbar.update(1)
     pbar.close()
     if split != 'test':
-        plt.hist(mses, bins=60)
-        plt.title(f'JPEG MSE for {split} {label}')
-        plt.xlabel('MSE')
-        plt.ylabel('Frequency')
-        plt.savefig(
-            f'visualisation/complexity/casting/{split}_{label}_hist.png')
-        plt.close()
-    with open('data/processed/tabular/complexity.csv', 'a') as f:
+        ax.hist(mses, bins=50, label=f'{split} {label}', alpha=0.7,
+                color=colours[(split, label)])
+
+    with open(os.path.join(tabular_data_dir, file_name), 'a') as f:
         for file, mse in zip(files, mses):
             f.write(f'{file},{mse},{split},{label}\n')
+
+ax.set_xlabel('MSE')
+# Format the x-axis with powers of 10
+ax.xaxis.set_major_formatter(
+    ticker.FuncFormatter(lambda x, _: f"$10^{{{int(np.log10(x))}}}$"))
+ax.set_ylabel('Frequency')
+ax.legend()
+fig_dir = os.path.join('visualisation', DATASET)
+create_dir(fig_dir)
+
+plt.savefig(os.path.join(fig_dir, 'complexity_hist.png'))
